e23bfdb02b108a82a0c50ce2265d6eae3813cfd3
EC2 Default User <ec2-user@ip-172-31-86-29.ec2.internal>
2019-12-15 12:30:45 +0000

    DRY

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,5 @@
 class TasksController < ApplicationController
+    before_action :set_task , only: [:show,:edit,:update,:destroy]
     def index
         @tasks=Task.all
     end
@@ -51,5 +52,7 @@ class TasksController < ApplicationController
     def task_params
        params.require(:task).permit(:content) 
     end
-    
+    def set_task
+        @task=Task.find(params[:id])
+    end
 end
