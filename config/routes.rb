da93b8061a7a2c09ffa730ac6f7a7b1a7840aafd
EC2 Default User <ec2-user@ip-172-31-86-29.ec2.internal>
2019-12-18 08:38:31 +0000

    addcolumn

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,8 @@ Rails.application.routes.draw do
     root to: 'tasks#index'
     get "signup",to: "users#new"
     post "users",to: "users#create"
+    get 'login', to: 'sessions#new'
+    post 'login', to: 'sessions#create'
+    delete 'logout', to: 'sessions#destroy'
     
 end
